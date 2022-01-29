64e0b88bc4f484d73b94a6060cda948c90bd78dd
LukasLtu186 <98620961+LukasLtu186@users.noreply.github.com>
2022-01-29 19:07:36 +0200

    Add files via upload

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 moveVector;
+    public CharacterController characterController;
+
+    public float moveSpeed = 10;
+    public float jumpSpeed = 6;
+    public float gravity = 20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        if (characterController.isGrounded && Input.GetButton("Jump"))
+            moveVector.y = jumpSpeed;
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        characterController.Move(move * moveSpeed * Time.deltaTime);
+
+        moveVector.y -= gravity * Time.deltaTime;
+        characterController.Move(moveVector * Time.deltaTime);
+    }
+}
