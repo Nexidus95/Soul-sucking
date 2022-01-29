f78752ace0b33626e12aa1ef466e22b73a416a87
Srdan <srdan.pavlovic87@gmail.com>
2022-01-29 15:00:41 +0100

    Added weapon models and rotate script to Tutorial scene

---
+++ Assets/Scripts/WeaponRotate.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class WeaponRotate : MonoBehaviour
+{
+
+    private float desiredRot;
+    public float rotSpeed = 250;
+    public float damping = 10;
+
+    private void OnEnable()
+    {
+        desiredRot = transform.eulerAngles.z;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Input.mousePosition.x > Screen.width / 2) desiredRot -= rotSpeed * Time.deltaTime;
+            else desiredRot += rotSpeed * Time.deltaTime;
+        }
+
+        var desiredRotQ = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, desiredRot);
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotQ, Time.deltaTime * damping);
+    }
+}
