addf4bca2835e71605be0d9846a112ef72283716
Nexidus95 <krisiunas1@gmail.com>
2022-01-30 01:53:46 +0200

    scene

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void MainMenu()
+    {
+        Application.LoadLevel(1);
+    }
+}
