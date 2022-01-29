b3c1a352a58c12553614f6cc45a027593a1a2eac
Srdan <srdan.pavlovic87@gmail.com>
2022-01-29 13:21:44 +0100

    Added Scifi assets

---
+++ Assets/Scripts/EnemyLook.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLook : MonoBehaviour
+{
+    public GameObject ThePlayer;
+
+    void Update()
+    {
+        transform.LookAt(ThePlayer.transform);    
+    }
+}
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public GameObject TheZombie;
+    public int EnemyHealth = 15;
+
+    void DeductPoints(int DamageAmount)
+    {
+        EnemyHealth -= DamageAmount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+/*        if (EnemyHealth <= 0)
+        {
+            this.GetComponent<ZombieFollow>().enabled = false;
+            StartCoroutine(ZombieDeath());
+        }*/
+    }
+
+    IEnumerator ZombieDeath()
+    {
+        TheZombie.GetComponent<Animation>().Play("Dying");
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+
+}
